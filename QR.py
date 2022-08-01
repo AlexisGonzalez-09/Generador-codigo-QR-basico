74f04e6e742b767ed18efe44007082ea5586ffff
Alexis Gonzalez <105887972+Nogitsune-09@users.noreply.github.com>
2022-07-31 21:51:35 -0400

    Update and rename GUI.py to QR.py

--- QR.py
+++ QR.py
@@ -1,10 +1,10 @@
 import qrcode # Importamos el modulo necesario para trabajar con codigos QR
-
+#para que funcione debes tener el archivo dentro de una carpeta y ejecurarlo y asi se guardara el codigo QR
 """data = ("https://www.instagram.com/young__nogitsune/")"""
 
 
 imagen = qrcode.make('https://www.instagram.com/young__nogitsune/')  # Creamos un codigo a partir de una cadena de texto
 
 archivo_imagen = open('codigo.png', 'wb')
-imagen.save(archivo_imagen)
+imagen.save(archivo_imagen) #Guarda el png en la carpeta
 archivo_imagen.close()
