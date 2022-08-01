7c804d6daa68238c32e8d7d3b6dbce62a3870ebb
Alexis Gonzalez <105887972+Nogitsune-09@users.noreply.github.com>
2022-07-31 21:55:32 -0400

    Update QR.py

--- QR.py
+++ QR.py
@@ -1,8 +1,6 @@
-import qrcode # Importamos el modulo necesario para trabajar con codigos QR
+import qrcode # Importacion del modulo necesario para trabajar con codigos QR
+
 #para que funcione debes tener el archivo dentro de una carpeta y ejecurarlo y asi se guardara el codigo QR
-"""data = ("https://www.instagram.com/young__nogitsune/")"""
-
-
 imagen = qrcode.make('https://www.instagram.com/young__nogitsune/')  # Creamos un codigo a partir de una cadena de texto
 
 archivo_imagen = open('codigo.png', 'wb')
